665130aa21018da6c36f9a65e376109060cac4b6
Marie <silvermanmarie19@gmail.com>
2026-01-28 15:34:11 -0500

    Exo01 v1

--- exo1.py
+++ exo1.py
@@ -23,11 +23,30 @@ Prompts EXACTS a utiliser :
 """
 
 # TODO: Lire le nom (str)
-
+nom = str(input("Entrez votre nom complet : "))
 # TODO: Lire les 4 valeurs (int)
-
+try:
+    match_football = int(input("Entrez le nombre de matchs de football des Carabins suivis cet automne : "))
+    duree_football = int(input("Entrez la duree moyenne d'un match de football suivi (en minutes) : "))
+    match_soccer = int(input("Entrez le nombre de matchs de soccer feminin des Carabins suivis cet automne : "))
+    duree_soccer = int(input("Entrez la duree moyenne d'un match de soccer suivi (en minutes) : "))
+except ValueError:
+    print("Erreur - donnees invalides.")
 # TODO: Valider les donnees (matchs >= 0, durees > 0)
-
+try:
+    if match_football or match_soccer < 0 : print("Erreur - donnees invalides.")
+    if duree_football or duree_soccer <= 0 : print("Erreur - donnees invalides.")
+except ValueError:
+    print("Erreur - donnees invalides.")
 # TODO: Calculer les minutes totales (football, soccer, total)
-
+total_football = match_football*duree_football
+total_soccer = match_soccer*duree_soccer
+total = total_soccer + total_football
 # TODO: Convertir en heures/minutes et afficher exactement 4 lignes
+watch_football = f"{int(total_football/60)}h{total_football%60}min"
+watch_soccer = f"{int(total_soccer/60)}h{total_soccer%60}min"
+watch = f"{int(total/60)}h{total%60}"
+print(f"""Bonjour {nom}
+      Football (Carabins): {match_football} match(s), {watch_football} de visionnage'
+      Soccer (Carabins): {match_soccer} match(s), {watch_soccer} de visionnage\
+      Total: {watch}""")
